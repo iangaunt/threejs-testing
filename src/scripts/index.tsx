890a141b080ff20f7d85ee3ecf3f2a5a246f2a24
Ian G. <iangaunt999@gmail.com>
2024-06-26 16:23:03 -0700

    Added three stuff

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -11,74 +11,89 @@ import { TTFLoader } from "three/examples/jsm/loaders/TTFLoader";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-function App() {
-    return (
-        <div className="container">
-            <h1>Ian's React Skeleton</h1>
-            <h2>Feel free to begin editing <span>index.tsx</span>.</h2>
-        </div>
-    )
-}
-
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x222222 );
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 4;
 
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 20;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 0x000000, 0 );
 document.body.appendChild( renderer.domElement );
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set( 0, 0, 0 );
+// const controls = new OrbitControls(camera, renderer.domElement)
+// controls.target.set( 0, 0, 0 );
 
-const teapot = new THREE.Mesh(
-    new TeapotGeometry(1.5, 15),
-    new THREE.MeshStandardMaterial( { color: 0xffffff, normalMap: new THREE.TextureLoader().load( "metal.jpg" )})
-);
-teapot.position.set(0, 0, 0);
-scene.add(teapot);
+const dl = new THREE.DirectionalLight(0xffffff, 3);
+dl.position.set(0, 5, 5);
+scene.add( dl );
 
-const floor = new THREE.Mesh( 
-    new THREE.BoxGeometry(15, 0.5, 15), 
-    new THREE.MeshStandardMaterial( { color: 0xcccccc })
-);
-floor.position.set(0, -3, 0);
-scene.add( floor ); 
-
-const sl = new THREE.SpotLight(0x0000ff, 100, 7, Math.PI / 5, 0.1);
-sl.position.set(0, 4, 0)
-const slHelper = new THREE.SpotLightHelper(sl);
-scene.add( sl, slHelper );
-
-const dl = new THREE.DirectionalLight(0x00ff00, 1);
-dl.position.set(0, 5, 0);
-const dlHelper = new THREE.DirectionalLightHelper(dl);
-scene.add( dl, dlHelper );
-
-/*
+const ttfLoader = new TTFLoader();
 const fontLoader = new FontLoader();
-fontLoader.load(
-    "optimer_regular.typeface.json",
-    (droidFont) => {
-        const textMesh = new THREE.Mesh(
+
+let pivot, textMesh;
+let canMove = false;
+let boxY = 0;
+
+ttfLoader.load(
+    "Mona-Sans.ttf",
+    (json) => {
+        const font = fontLoader.parse(json);
+        textMesh = new THREE.Mesh(
             new TextGeometry("i9nn", {
-                height: 2,
+                depth: 2,
                 size: 10,
-                font: droidFont
+                font: font,
+
+                bevelEnabled: true,
+		        bevelThickness: 1,
+                bevelSize: 0.4,
+                bevelOffset: 0,
+                bevelSegments: 20
+
             }),
-            new THREE.MeshNormalMaterial()
+            new THREE.MeshStandardMaterial({ 
+                color: 0xffffff,
+            })
         );
-        textMesh.position.set(0, 0, 0);
-        scene.add(textMesh);
+       
+        textMesh.geometry.computeBoundingBox();
+        textMesh.scale.z = 0.2;
+
+        let vec: THREE.Vector3;
+        const box = textMesh.geometry.boundingBox.getSize(new THREE.Vector3);
+        
+        textMesh.position.set(-box.x / 2, -box.y / 2, 0)
+        boxY = -box.y / 2;
+
+        pivot = new THREE.Group();
+        scene.add(pivot);
+        pivot.add(textMesh);
+
+        canMove = true;
     }
 )
-*/
 
+let pointerX = 0;
+let pointerY = 0;
+
+window.addEventListener("mousemove", (e: MouseEvent) => {
+    pointerX = e.clientX;
+    pointerY = e.clientY;
+});
+
+let t = 0;
 function animate() {
-    teapot.rotation.x += 0.025;
-    teapot.rotation.y += 0.025;
+    if (canMove) {
+        pivot.rotation.set(
+            (pointerY - window.innerHeight / 2) / 2000, 
+            (pointerX - window.innerWidth / 2) / 2000, 
+            0
+        );
+        pivot.position.y = Math.sin(t) / 2 + 0.5;
+    }
 
 	renderer.render( scene, camera );
+    t += 0.01;
 }
 renderer.setAnimationLoop( animate );
